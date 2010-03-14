7eeb4569fb2ceba0d4fd8ee3d990ce2daf15b562
Dan Wilson <dan@timetric.com>
2010-03-14 18:53:18 +0000

    Initial import

---
+++ main.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from google.appengine.api import urlfetch
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import util
+from google.appengine.ext.webapp import template
+
+import os
+import datetime
+import icalendar
+
+ical_url = "http://www.arsenal.com/_scripts/ical.ics?tid=1006&sid=117"
+
+
+class MainHandler(webapp.RequestHandler):
+
+    def get(self):
+        result = urlfetch.fetch(ical_url)
+        cal = icalendar.Calendar.from_string(result.content)
+        games = []
+        for c in cal.walk('VEVENT'):
+            try:
+                if c["LOCATION"] == "Emirates Stadium" and c["DTSTART"].dt.date() > datetime.date.today():
+                    games.append({"StartTime":c["DTSTART"].dt, "Summary": c["SUMMARY"]})
+                    
+            except KeyError:
+                pass
+        path = os.path.join(os.path.dirname(__file__), 'index.html')
+        self.response.out.write(template.render(path, {"games":games}))
+
+def main():
+    application = webapp.WSGIApplication([('/', MainHandler)],
+                                       debug=True)
+    util.run_wsgi_app(application)
+
+
+if __name__ == '__main__':
+    main()
