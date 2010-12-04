8146dbdb9b8dc31679f78c04001f9b7c788e70b3
Dan Wilson <dan@timetric.com>
2010-12-04 16:19:30 +0000

    Grown the ability to tweet

--- main.py
+++ main.py
@@ -22,8 +22,10 @@ from google.appengine.ext.webapp import template
 
 import os
 import datetime
+import logging
 import icalendar
 from pytz.gae import pytz
+import tweepy
 
 ical_url = "http://www.arsenal.com/_scripts/ical.ics?tid=1006&sid=118"
 london = pytz.timezone("Europe/London")
@@ -38,7 +40,7 @@ class MainHandler(webapp.RequestHandler):
         for c in cal.walk('VEVENT'):
             try:
                 if c["LOCATION"] == "Emirates Stadium" and c["DTSTART"].dt.date() >= datetime.date.today():
-                    games.append({"StartTime":c["DTSTART"].dt.astimezone(london), "Summary": c["SUMMARY"]})
+                    games.append({"StartTime":c["DTSTART"].dt.astimezone(london), "Summary": c["SUMMARY"].replace("(H)","")})
                     if c["DTSTART"].dt.date() == datetime.date.today():
                         today = True
             except KeyError:
@@ -46,8 +48,38 @@ class MainHandler(webapp.RequestHandler):
         path = os.path.join(os.path.dirname(__file__), 'index.html')
         self.response.out.write(template.render(path, {"games":games, "today":today}))
 
+class TweetHandler(webapp.RequestHandler):
+    
+    def get(self):
+        logging.info('Daily tweet')
+        
+        result = urlfetch.fetch(ical_url)
+        cal = icalendar.Calendar.from_string(result.content)
+        games = []
+        today = False
+        for c in cal.walk('VEVENT'):
+            try:
+                if c["LOCATION"] == "Emirates Stadium" and c["DTSTART"].dt.date() == datetime.date.today():
+                    self.tweet("Arsenal home game today at %s (%s)" % (c["DTSTART"].dt.astimezone(london).strftime("%H:%M"), c["SUMMARY"].replace(" (H)","")))
+            except KeyError:
+                pass
+        
+        self.response.set_status(204)
+        return self.response
+        
+    def tweet(self, message):
+        logging.debug("TWEET: %s", message)
+        
+        auth = tweepy.OAuthHandler(twitter_consumer_key, twitter_consumer_secret)
+        auth.set_access_token(twitter_access_key, twitter_access_secret)
+        
+        api = tweepy.API(auth)
+        api.update_status(message)
+        
+
 def main():
-    application = webapp.WSGIApplication([('/', MainHandler)],
+    logging.getLogger().setLevel(logging.DEBUG)
+    application = webapp.WSGIApplication([('/', MainHandler), ('/tweet/matchday', TweetHandler)],
                                        debug=True)
     util.run_wsgi_app(application)
 
